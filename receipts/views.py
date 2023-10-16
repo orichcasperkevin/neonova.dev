62d9ca91c0759ece610c110da5d22d3343ed7827
orichcasperkevin <orichcaspertheghost@gmail.com>
2023-10-16 09:40:51 +0300

    test reciepts using fixtures

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# receipts/views.py
+from django.http import JsonResponse
+from receipts.models import Receipt
 
-# Create your views here.
+def receipt_json(request):
+    results = {
+        "receipts":[],
+    }
+
+    for receipt in Receipt.objects.all():
+        line = [str(receipt), []]
+        for item in receipt.item_set.all():
+            line[1].append(str(item))
+
+        results["receipts"].append(line)
+
+    return JsonResponse(results)
